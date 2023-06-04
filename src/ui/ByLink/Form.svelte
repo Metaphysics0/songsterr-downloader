a2614680b7b943b406b6743678093e7692115591
Ryan Roberts <ryanr@cityhive.net>
2023-06-04 11:01:36 +0300

    disable if needed

--- src/ui/ByLink/Form.svelte
+++ src/ui/ByLink/Form.svelte
@@ -16,6 +16,15 @@
 		}
 		triggerFileDownloadFromSongsterrResponse(res);
 	}
+
+	function checkIfInputIsValid(event: Event): void {
+		const { value } = event.target as HTMLInputElement;
+		isValid = SONGSTERR_URL_REGEX_PATTERN.test(value);
+
+		clearValidationMessage(event);
+	}
+
+	let isValid: boolean = false;
 </script>
 
 <form
@@ -38,14 +47,15 @@
 			pattern={SONGSTERR_URL_REGEX_PATTERN.source}
 			required
 			on:invalid={setValidationMessage}
-			on:input={clearValidationMessage}
+			on:input={checkIfInputIsValid}
 			placeholder="https://www.songsterr.com/a/wsa/structures-hydroplaning-tab-s88503"
 			class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2 text-center"
 		/>
 	</label>
 	<button
-		class="w-fit px-2 py-1 font-semibold p-2 rounded-lg shadow-md transition duration-75 cursor-pointer bg-red-500 hover:bg-red-400 text-white"
-		>Download Tab!</button
+		disabled={!isValid}
+		class="w-fit px-2 py-1 font-semibold p-2 rounded-lg shadow-md transition duration-75 cursor-pointer bg-red-500 hover:bg-red-400 text-white disabled:bg-slate-5 disabled:hover:bg-slate-6 disabled:hover:cursor-not-allowed"
+		>Get Download Link!</button
 	>
 </form>
 
