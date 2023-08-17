729efa294bfd2965ca84e6786b64c506b5cfe49f
Ryan Roberts <47161696+Metaphysics0@users.noreply.github.com>
2023-08-17 10:28:37 +0300

    add vercel analytics (#10)
    Co-authored-by: Ryan Roberts <ryanr@cityhive.net>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+  import { inject } from '@vercel/analytics';
+  import { dev } from '$app/environment';
   import 'uno.css';
   import '@unocss/reset/tailwind.css';
+
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <nav class="mb-8" />
