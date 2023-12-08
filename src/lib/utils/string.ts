1caf2575150b10883a7bd93fdd033201d1ec9840
Ryan Roberts <47161696+Metaphysics0@users.noreply.github.com>
2023-12-08 16:43:39 +0200

    fix empty song names when not english (#26)

--- src/lib/utils/string.ts
+++ src/lib/utils/string.ts
@@ -3,6 +3,11 @@
  * output: "bubble-dream-tab-book-version-231"
  */
 export function normalize(input: string) {
+  if (/[^a-zA-Z0-9\s]/.test(input)) {
+    // If non-English characters are found, return the input string as is
+    return input;
+  }
+
   let normalized = input.toLowerCase();
   normalized = normalized.replace(/[^a-z0-9\s]/g, ' ');
   normalized = normalized.replace(/\s+/g, ' ');
