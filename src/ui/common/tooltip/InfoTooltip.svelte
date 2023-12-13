060bae86ee892d71163d2928f07457dcce3498c5
ryan <ryanroberts562@gmail.com>
2023-12-13 21:41:19 +0200

    progress on working on modal

---
+++ src/ui/common/tooltip/InfoTooltip.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import Icon from '@iconify/svelte';
+  import { createTooltip, melt } from '@melt-ui/svelte';
+  import { fade } from 'svelte/transition';
+
+  const {
+    elements: { trigger, content, arrow },
+    states: { open }
+  } = createTooltip({
+    positioning: {
+      placement: 'top'
+    },
+    openDelay: 0,
+    closeDelay: 0,
+    closeOnPointerDown: false,
+    forceVisible: true
+  });
+</script>
+
+<button type="button" class="trigger" use:melt={$trigger} aria-label="Add">
+  <Icon icon="material-symbols:info" />
+</button>
+
+{#if $open}
+  <div
+    use:melt={$content}
+    transition:fade={{ duration: 100 }}
+    class="z-10 rounded-lg bg-white shadow"
+  >
+    <div use:melt={$arrow} />
+    <p class="px-4 py-1 text-magnum-700">Add item to library</p>
+  </div>
+{/if}
+
+<style lang="postcss">
+  .trigger {
+    @apply inline-flex h-9 w-9 items-center justify-center rounded-full bg-white;
+    @apply text-magnum-900 transition-colors hover:bg-white/90;
+    @apply focus-visible:ring focus-visible:ring-magnum-400 focus-visible:ring-offset-2;
+    @apply p-0 text-sm font-medium;
+  }
+</style>
