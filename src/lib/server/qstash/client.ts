162aa884d1be72bee90f80f1d9995e7da06f33b0
ryan <ryanroberts562@gmail.com>
2023-12-20 08:37:58 +0200

    update import

--- src/lib/server/qstash/client.ts
+++ src/lib/server/qstash/client.ts
@@ -1,5 +1,5 @@
 import { QSTASH_TOKEN } from '$env/static/private';
-import { Client } from '@upstash/qstash/.';
+import { Client } from '@upstash/qstash';
 
 export default new Client({
   token: QSTASH_TOKEN
