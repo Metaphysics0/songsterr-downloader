f5845d40a1ebcce920ff9bba47faba9631347cf3
Ryan Roberts <47161696+Metaphysics0@users.noreply.github.com>
2024-06-06 21:22:53 +0300

    feat: enable bulk download againn (#39)

--- src/ui/general/Header.svelte
+++ src/ui/general/Header.svelte
@@ -27,12 +27,12 @@
     <p>
       {headerText} from
       <Link href="https://songsterr.com" innerText="Songsterr" />
-      {#if selectedTabMenuIndex === 0}
+      <!-- {#if selectedTabMenuIndex === 0}
         or <Link
           href="https://www.ultimate-guitar.com/"
           innerText="Ultimate Guitar"
         />
-      {/if}
+      {/if} -->
       and click the button to get a
       <Link href="https://www.guitar-pro.com/" innerText="Guitar Pro" />
       file!
