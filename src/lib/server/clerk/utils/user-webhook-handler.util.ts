c1846b55992073f5a109af35b0fe9a4d51724c0e
ryan <ryanroberts562@gmail.com>
2024-08-03 20:37:53 +0000

    feat: user webhook finish

--- src/lib/server/clerk/utils/user-webhook-handler.util.ts
+++ src/lib/server/clerk/utils/user-webhook-handler.util.ts
@@ -1,36 +1,85 @@
-import { WebhookEvent } from '../types/clerk.types';
+import prisma from '$lib/server/prisma';
+import { logger } from '$lib/utils/logger';
+import { UserWebhookEvent } from '../types/clerk.types';
+import { UserJSON } from '../types/json.types';
 
 export class ClerkUserWebhookHandler {
-  private readonly webhookEvent: WebhookEvent;
-  constructor(webhookEvent: WebhookEvent) {
+  private webhookEvent: UserWebhookEvent;
+  private clientIpAddress: string;
+  constructor({
+    webhookEvent,
+    clientIpAddress
+  }: {
+    webhookEvent: UserWebhookEvent;
+    clientIpAddress: string;
+  }) {
     this.webhookEvent = webhookEvent;
+    this.clientIpAddress = clientIpAddress;
   }
 
   public async handleWebhook(): Promise<void> {
+    logger.info(
+      `Clerk UserWebhookHandler - Beginning webhook for ${this.webhookEvent.type}`
+    );
     switch (this.webhookEvent.type) {
       case 'user.created':
-        await this.userCreated();
       case 'user.updated':
-        await this.userUpdated();
+        await this.upsertUser();
       case 'user.deleted':
-        await this.userDeleted();
+        await this.deleteUser();
       default:
         break;
     }
   }
 
-  private async userCreated(): Promise<any> {
-    console.log('user created');
+  private async upsertUser(): Promise<void> {
+    await prisma.user.upsert({
+      where: {
+        ipAddress: this.clientIpAddress
+      },
+      update: {
+        email: this.userPrimaryEmailAddress,
+        userMetadata: JSON.stringify(this.webhookEvent.data)
+      },
+      create: {
+        ipAddress: this.clientIpAddress,
+        email: this.userPrimaryEmailAddress,
+        userMetadata: JSON.stringify(this.webhookEvent.data)
+      }
+    });
   }
 
-  private async userUpdated(): Promise<void> {
-    console.log('user updated');
+  private async deleteUser(): Promise<void> {
+    const user = await prisma.user.findFirst({
+      where: { email: this.userPrimaryEmailAddress }
+    });
+    if (!user) {
+      logger.warn(
+        `ClerkUserWebhookHanlder - Delete User - Unable to find user from webhook: ${JSON.stringify(
+          this.webhookEvent
+        )}`
+      );
+      return;
+    }
+
+    await prisma.user.delete({ where: { id: user.id } });
   }
 
-  private async userDeleted(): Promise<void> {
-    console.log('USER DELETED WEBHOOK');
+  private get userPrimaryEmailAddress() {
+    const webhookData = this.webhookEvent.data as UserJSON;
+    const {
+      email_addresses: emailAddressObjects,
+      primary_email_address_id: primaryEmailAddressId
+    } = webhookData;
+
+    const primaryEmailAddress = emailAddressObjects.find(
+      (emailAddressObject) => emailAddressObject.id === primaryEmailAddressId
+    );
+
+    if (!primaryEmailAddress) {
+      return emailAddressObjects[0].email_address;
+    }
+
+    return primaryEmailAddress.email_address;
   }
 }
-export async function handleUserWebhook(
-  webhookEvent: WebhookEvent
-): Promise<void> {}
