0c51f62219da2c72a5e068cc49af96f3728a738b
ryan <ryanroberts562@gmail.com>
2024-08-03 17:54:50 +0000

    feat: basic user auth with Clerk

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,11 @@
   import { SvelteToast } from '@zerodevx/svelte-toast';
   import Header from '../ui/general/Header.svelte';
   import { amountOfDownloadsAvailable } from '../stores/amountOfDownloadsAvailable.store';
+  import UserButton from 'clerk-sveltekit/client/UserButton.svelte';
+  import SignedIn from 'clerk-sveltekit/client/SignedIn.svelte';
+  import SignedOut from 'clerk-sveltekit/client/SignedOut.svelte';
+  import SignInButton from 'clerk-sveltekit/client/SignInButton.svelte';
+  import Icon from '@iconify/svelte';
 
   inject({ mode: dev ? 'development' : 'production' });
 
@@ -18,6 +23,18 @@
 
 <SvelteToast />
 
-<nav class="mb-8" />
+<nav class="p-2 flex items-end justify-end">
+  <SignedIn>
+    <UserButton afterSignOutUrl="/" />
+  </SignedIn>
+  <SignedOut>
+    <SignInButton redirectUrl="/" mode="modal" class="p-3">
+      <Icon
+        icon="fa6-solid:circle-user"
+        class="text-3xl cursor-pointer opacity-70 hover:opacity-100"
+      />
+    </SignInButton>
+  </SignedOut>
+</nav>
 <Header />
 <slot />
