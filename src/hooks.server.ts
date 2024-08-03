1714c5e5ccff897d523dd397960f712bc78829e3
ryan <ryanroberts562@gmail.com>
2024-08-03 17:54:50 +0000

    feat: basic user auth with Clerk

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,8 @@
+import { CLERK_SECRET_KEY } from '$env/static/private';
 import { start_prisma } from '$lib/server/prisma';
+import type { Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
+import { handleClerk } from 'clerk-sveltekit/server';
 
 start_prisma()
   .then(() => {
@@ -7,3 +11,11 @@ start_prisma()
   .catch((e: any) => {
     console.error('🔴 Error starting prisma', e);
   });
+
+export const handle: Handle = sequence(
+  handleClerk(CLERK_SECRET_KEY, {
+    debug: true,
+    protectedPaths: ['/admin'],
+    signInUrl: '/sign-in'
+  })
+);
