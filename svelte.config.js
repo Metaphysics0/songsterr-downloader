fc67d1d2336bce3b31acdc89dc3b5f96725e24a6
ryan <ryanroberts562@gmail.com>
2025-01-18 11:48:27 +0000

    chore: remove melt

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,10 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import { preprocessMeltUI } from '@melt-ui/pp';
 import sequence from 'svelte-sequential-preprocessor';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
+  preprocess: sequence([vitePreprocess()]),
 
   kit: {
     adapter: adapter()
