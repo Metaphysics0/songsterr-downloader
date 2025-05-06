59fbdd14ef465dc5893becb04455b3ae80b1bf78
ryan <ryanroberts562@gmail.com>
2025-05-06 14:14:50 +0000

    feat: update temporarily down modal

--- src/ui/modals/TemporarilyDownModal.svelte
+++ src/ui/modals/TemporarilyDownModal.svelte
@@ -4,6 +4,22 @@
 
 <dialog open={$temporarilyDownModalStore}>
   <h2>We're temporarily down</h2>
-  <p>We're currently down for maintenance. We'll be back online shortly.</p>
+  <p>
+    Songsterr has recently changed their API, causing this app and similar apps
+    to no longer be functioning ):
+  </p>
+  <p>
+    Luckily, songsterr updated the UI/UX for downloading tabs, so this app does
+    not have much of a purpose anymore.
+  </p>
+  <p>
+    Thank you to everyone that has used this app, and for all the kind words and
+    support.
+  </p>
+  <p>
+    Feel free to donate a cup of coffee to me if you'd like to support me
+    continuing to work on this app.
+  </p>
+  <p>Take care, and stay safe.</p>
   <button on:click={() => ($temporarilyDownModalStore = false)}>Close</button>
 </dialog>
