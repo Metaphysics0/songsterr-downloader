c60242d6df4f0c39350c955ee20a6826cea5864b
Ryan Roberts <ryanroberts562@gmail.com>
2025-09-17 15:48:11 +0300

    feat: move to bun

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
-  import { inject } from '@vercel/analytics';
-  import { dev } from '$app/environment';
   import 'uno.css';
   import '@unocss/reset/tailwind.css';
 
   import { SvelteToast } from '@zerodevx/svelte-toast';
   import Header from '../ui/common/Header.svelte';
   import TemporarilyDownModal from '../ui/modals/TemporarilyDownModal.svelte';
-
-  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <SvelteToast />
