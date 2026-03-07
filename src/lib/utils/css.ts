559a53b4790e64871e78f6d01420f16edaa00dc1
Ryan Roberts <ryanroberts562@gmail.com>
2026-03-07 08:36:16 +0200

    feat: remove package lock

--- src/lib/utils/css.ts
+++ src/lib/utils/css.ts
@@ -11,14 +11,9 @@ export function styleToString(
   }, '');
 }
 
-const sharedBtnStyle =
-  'w-fit px-2 py-1 font-semibold p-2 rounded-lg shadow-md transition duration-75 cursor-pointer';
-
 export const commonCssClasses = {
   textInput:
     'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-2 text-center',
   getTabButton:
     'w-fit px-2 py-1 font-semibold p-2 rounded-lg shadow-md transition duration-75 cursor-pointer bg-red-500 hover:bg-red-400 text-white disabled:bg-slate-5 disabled:hover:bg-slate-6 disabled:hover:cursor-not-allowed'
-  // downloadBtn: sharedBtnStyle + ' bg-red-500 hover:bg-red-400 text-white',
-  // disabledBtn: sharedBtnStyle + ' opacity-50 cursor-not-allowed'
 };
