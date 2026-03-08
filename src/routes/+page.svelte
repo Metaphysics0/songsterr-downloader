817c2edbe6c17ff13646e2ffc0cace060949ea5c
Ryan Roberts <ryanroberts562@gmail.com>
2026-03-08 12:51:17 +0200

    chore: add social links back

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,15 @@
 <script>
   import Form from '$lib/components/Form.svelte';
+  import SocialLinks from '$lib/components/SocialLinks.svelte';
+  import { formState } from '$lib/runes/form-state.svelte';
+  import { fade } from 'svelte/transition';
 </script>
 
 <main class="max-w-md mx-auto">
   <Form />
+  {#if !formState.selectedSong && !formState.isLoadingMetadata}
+    <div class="mt-8" transition:fade={{ duration: 200 }}>
+      <SocialLinks />
+    </div>
+  {/if}
 </main>
