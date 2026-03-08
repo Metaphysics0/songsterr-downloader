54bf95cb6a5795e1b50b8b25ded4e3e36126fdc7
Ryan Roberts <ryanroberts562@gmail.com>
2026-03-08 11:14:56 +0200

    chore: ts

--- src/lib/analytics/mixpanel.ts
+++ src/lib/analytics/mixpanel.ts
@@ -5,18 +5,16 @@ mixpanel.init('09282cac571bcb88db2bd0cfa8a5c5f7', {
   record_sessions_percent: 0
 });
 
-export function trackGuitarProDownloaded(props: {
+interface TrackDownloadParams {
   title: string;
   artist: string;
   songId: number;
-}) {
+}
+
+export function trackGuitarProDownloaded(props: TrackDownloadParams) {
   mixpanel.track('Guitar Pro Downloaded', props);
 }
 
-export function trackMidiDownloaded(props: {
-  title: string;
-  artist: string;
-  songId: number;
-}) {
+export function trackMidiDownloaded(props: TrackDownloadParams) {
   mixpanel.track('MIDI Downloaded', props);
 }
