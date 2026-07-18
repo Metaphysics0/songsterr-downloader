eee5d77332f30f4ac186f47ee0a5b0bc3bfe0d79
Ryan Roberts <47161696+Metaphysics0@users.noreply.github.com>
2026-07-18 11:59:50 +0300

    Feat/re advertise again lol (#93)
    * feat: fretwise ad init
    
    * feat: advertise my other app again lol sorry-not-sorry
    
    * feat: larger img
    
    * feat: add mixpanel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import Form from '$lib/components/Form.svelte';
   import SocialLinks from '$lib/components/SocialLinks.svelte';
+  import FretwiseAd from '$lib/components/FretwiseAd.svelte';
 </script>
 
 <main class="max-w-md mx-auto">
@@ -8,3 +9,7 @@
   <div class="mb-14"></div>
   <SocialLinks />
 </main>
+
+<footer class="flex flex-col items-center justify-center h-max">
+  <FretwiseAd />
+</footer>
