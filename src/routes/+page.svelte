890cf2fe16b91381a0e7fe86a7958a82746ee134
Ryan Roberts <ryanroberts562@gmail.com>
2026-08-09 17:57:21 +0300

    chore: remove add

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
   import Form from '$lib/components/Form.svelte';
   import SocialLinks from '$lib/components/SocialLinks.svelte';
-  import FretwiseAd from '$lib/components/FretwiseAd.svelte';
 </script>
 
 <main class="max-w-md mx-auto">
@@ -10,6 +9,6 @@
   <SocialLinks />
 </main>
 
-<footer class="flex flex-col items-center justify-center h-max">
+<!-- <footer class="flex flex-col items-center justify-center h-max">
   <FretwiseAd />
-</footer>
+</footer> -->
